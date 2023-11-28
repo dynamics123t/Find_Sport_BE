5b24920c701049661968db8fa94a4dbb40fdbae5
dynamics123t <tannguyen512001@gmail.com>
2023-11-28 17:00:18 +0700

    recomment

--- app/services/recomment.py
+++ app/services/recomment.py
@@ -17,11 +17,12 @@ class RecommentService:
     def create_recomnent_to_post(self, user_id: str, recomment_in: RecommentCreate):
         recomment_in.id = uuid.uuid4()
         recomment_in.id_user = user_id
-        result = recomment.create(db=self.db, obj_in=recomment_in, auto_commit=True)
+        result = recomment.create(db=self.db, obj_in=recomment_in)
+        self.db.commit()
         return RecommentResponse.from_orm(result)
 
     def update_recomment_of_user(self, user_id: str, recomment_in: RecommentUpdate):
-        comment_db = recomment.get(db=self.db, id=recomment_in.id)
+        comment_db = recomment.get(db=self.db, entry_id=recomment_in.id)
         if comment_db is None:
             raise HTTPException(status_code=400, detail="RECOMMENT NOT AVAILABLE")
         if comment_db.id_user != user_id:
@@ -32,16 +33,23 @@ class RecommentService:
         return recomment.get_count_recomment_by_commet(db=self.db, id_comment=id_recomment)
 
     def get_recomment_by_comment(self, id_comment: str, skip: int, limit: int):
-        data = recomment.get_recomment_by_comment(db=self.db, id_comment=id_comment, skip=skip, limit=limit)
-        response, count = []
+        data, count = recomment.get_recomment_by_comment(db=self.db, id_comment=id_comment, skip=skip, limit=limit)
+        response = []
         for item in data:
             response.append(RecommentResponse.from_orm(item))
         return response, count
 
+    # def get_comment_by_post(self, id_sport: str, skip: int, limit: int):
+    #     data, count = comment.get_comment_by_post(db=self.db, id_sport=id_sport, skip=skip, limit=limit)
+    #     response = []
+    #     for item in data:
+    #         response.append(CommentResponse.from_orm(item))
+    #     return response, count
+
     def remove_recomment_by_id(self, user: User, recomment_id: str):
-        data = recomment.get(db=self.db, id=recomment_id)
+        data = recomment.get(db=self.db, entry_id=recomment_id)
         if data.id_user != user.id:
             raise HTTPException(status_code=401, detail="ko phai nguoi so huu")
-        status = recomment.remove(db=self.db, id=recomment_id, auto_commit=True)
+        status = recomment.remove(db=self.db, entry_id=recomment_id)
         self.db.commit()
         return status
